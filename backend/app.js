a7609de6e034ce3cf2da3761174f834adab8db3a
Eric Khangati <ekhangati@gmail.com>
2023-07-16 17:29:46 +0300

    Updated CORS

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['https://tours-app-api.vercel.app'],
+    origin: ['https://tours-app-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PATCH'],
     credentials: true,
   })
