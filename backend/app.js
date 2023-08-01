06096cb0308ec39396d06c492c719171adb6adce
Eric Khangati <ekhangati@gmail.com>
2023-08-01 07:17:33 +0300

    Added hpp

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const toursRoutes = require('./routes/toursRoutes');
 const personsRoutes = require('./routes/personsRoutes');
@@ -22,6 +23,11 @@ app.use(mongoSanitize());
 app.use(xss());
 app.use(morgan('dev'));
 app.use(helmet());
+app.use(
+  hpp({
+    whitelist: ['duration', 'ratingAverage'],
+  })
+);
 
 const limiter = rateLimit({
   max: 100,
