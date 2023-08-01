08ca1e52c8379df3eb94ef1ee0f81f05a3cdfcb0
Eric Khangati <ekhangati@gmail.com>
2023-08-01 05:43:14 +0300

    Added rate limiter

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
 
 const toursRoutes = require('./routes/toursRoutes');
 const personsRoutes = require('./routes/personsRoutes');
@@ -16,6 +17,14 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('dev'));
+
+const limiter = rateLimit({
+  max: 100,
+  windowM: 60 * 60 * 1000,
+  message: 'Too many request from this IP',
+});
+
+app.use('/api', limiter);
 
 app.get('/', (req, res) => {
   res.json('Welcome to Tours API');
