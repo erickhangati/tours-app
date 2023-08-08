db4c055e3bc41c17a2ad77ebc841bd6db08a32ce
Eric Khangati <ekhangati@gmail.com>
2023-08-08 11:39:38 +0300

    Added review handler

--- backend/controllers/reviewsController.js
+++ backend/controllers/reviewsController.js
@@ -1,5 +1,15 @@
 const Review = require('../models/reviewsModel');
+const { catchAsync } = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getReviews = (req, res) => {
-  res.status(200).json({ status: 'success' });
-};
+exports.getReviews = catchAsync(async (req, res) => {
+  const reviews = await Review.find();
+  if (!reviews) return next(new AppError(`No reviews`), 404);
+  res.status(200).json({ status: 'success', data: { reviews } });
+});
+
+exports.createReview = catchAsync(async (req, res) => {
+  const review = await Review.create(req.body);
+  if (!review) return next(new AppError(`Something went wrong`), 500);
+  res.status(200).json({ status: 'success', data: { review } });
+});
--- backend/routes/reviewsRoutes.js
+++ backend/routes/reviewsRoutes.js
@@ -3,6 +3,9 @@ const reviewsController = require('../controllers/reviewsController.js');
 
 const router = express.Router();
 
-router.route('/').get(reviewsController.getReviews);
+router
+  .route('/')
+  .get(reviewsController.getReviews)
+  .post(reviewsController.createReview);
 
 module.exports = router;
