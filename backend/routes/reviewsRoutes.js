35a1aff444ed94dcbddc1435adc1f6341f97df3a
Eric Khangati <ekhangati@gmail.com>
2023-08-08 11:54:44 +0300

    Updated review router

--- backend/routes/reviewsRoutes.js
+++ backend/routes/reviewsRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const reviewsController = require('../controllers/reviewsController.js');
+const authController = require('../controllers/authController.js');
 
 const router = express.Router();
 
 router
   .route('/')
-  .get(reviewsController.getReviews)
-  .post(reviewsController.createReview);
+  .get(authController.protect, reviewsController.getReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewsController.createReview
+  );
 
 module.exports = router;
