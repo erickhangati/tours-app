5930fd4067c93f6329adff81ff5154dce9f9e879
Eric Khangati <ekhangati@gmail.com>
2023-08-11 07:47:25 +0300

    Added createOne  handler factory

--- backend/controllers/handlerFactory.js
+++ backend/controllers/handlerFactory.js
@@ -1,6 +1,16 @@
 const AppError = require('../utils/appError');
 const { catchAsync } = require('../utils/catchAsync');
 
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: doc,
+    });
+  });
+
 exports.updateOne = (Model) =>
   catchAsync(async (req, res, next) => {
     let id;
